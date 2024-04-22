97f15fe4fb9752f07e499419f35485beb1b9e6ba
daiana-besterekova <85840329+daiana-besterekova@users.noreply.github.com>
2024-04-22 21:08:14 +0100

    Added search bar in RecipeView

--- YumYard/Recipe API.swift
+++ YumYard/Recipe API.swift
@@ -10,9 +10,9 @@ import Foundation
 class apiCall: ObservableObject {
     @Published var foods = [Recipe]()
 
-    func getRecipe(completion: @escaping ([Recipe]) -> ()) {
-        let query = "italian wedding soup".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
-        let url = URL(string: "https://api.api-ninjas.com/v1/recipe?query=" + (query ?? ""))!
+    func getRecipe(query: String, completion: @escaping ([Recipe]) -> ()) {
+        let queryEncoded = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        let url = URL(string: "https://api.api-ninjas.com/v1/recipe?query=" + queryEncoded)!
         var request = URLRequest(url: url)
         request.setValue("c4DZ5e6Uq3RvCrR89OLnww==536rNPsLQWvwsPwG", forHTTPHeaderField: "X-Api-Key")
         let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
--- YumYard/RecipeView.swift
+++ YumYard/RecipeView.swift
@@ -6,10 +6,41 @@
 //
 
 import SwiftUI
-struct RecipeView: View {
-    @State var foods = [Recipe]()
+
+struct SearchBar: View {
+    @Binding var searchText: String
 
     var body: some View {
+        HStack {
+            TextField("Search", text: $searchText)
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .padding(.horizontal, 10)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 15)
+                    }
+                )
+        }
+    }
+}
+
+struct RecipeView: View {
+    @State var foods = [Recipe]()
+    @State private var searchText = ""
+    
+    // Define your default recipes here
+    @State private var defaultText = "Chicken Soup"
+
+    var body: some View {
+        VStack{
+            SearchBar(searchText: $searchText)
+
             List(foods) { food in
                 VStack(alignment: .leading) {
                     Text(food.title)
@@ -23,13 +54,19 @@ struct RecipeView: View {
                 }
             }
             .onAppear() {
-                apiCall().getRecipe { (foods) in
+                apiCall().getRecipe(query: defaultText) { (foods) in
                     self.foods = foods
                 }
             }
+            .onChange(of: searchText) {
+                oldText, newText in
+                apiCall().getRecipe(query: newText) { (foods) in
+                    self.foods = foods
+                }
+            }
+        }
     }
 }
-
 struct RecipeView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeView()
