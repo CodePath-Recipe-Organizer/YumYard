dec7072048ac21819939b95ca2c6ed7e4241f8bf
daiana-besterekova <85840329+daiana-besterekova@users.noreply.github.com>
2024-04-22 23:36:33 +0100

    Added Log out in AccountView page, added the heart button to add a recipe to Favorites

--- YumYard/RecipeView.swift
+++ YumYard/RecipeView.swift
@@ -15,7 +15,6 @@ struct SearchBar: View {
             TextField("Search", text: $searchText)
                 .padding(7)
                 .padding(.horizontal, 25)
-                .background(Color(.systemGray6))
                 .cornerRadius(8)
                 .padding(.horizontal, 10)
                 .overlay(
@@ -33,40 +32,56 @@ struct SearchBar: View {
 struct RecipeView: View {
     @State var foods = [Recipe]()
     @State private var searchText = ""
-    
     // Define your default recipes here
     @State private var defaultText = "Chicken Soup"
-
+    
     var body: some View {
         VStack{
             SearchBar(searchText: $searchText)
 
-            List(foods) { food in
+            List(foods.indices, id: \.self) { index in
+                let food = foods[index]
                 VStack(alignment: .leading) {
                     Text(food.title)
                         .font(.title)
                         .fontWeight(.bold)
-                    Text("\(food.ingredients)")
-                        .font(.subheadline)
-                        .fontWeight(.bold)
-                    Text("\(food.servings)")
-                        .font(.body)
+                    Text(food.ingredients)
+                    HStack {
+                        Text("\(food.servings)")
+                        Spacer()
+                        Button {
+                            toggleLikedState(for: index) // Toggle liked state for the specific item
+                            if foods[index].isLiked {
+                                sendRecipeToFirestore(recipe: foods[index]) // Send recipe to Firestore if liked
+                            }
+                        } label: {
+                            Image(systemName: food.isLiked ? "heart.fill" : "heart")
+                                .foregroundColor(food.isLiked ? .pink : .pink) // Change color based on liked state
+                        }
+                    }
                 }
+                .padding()
+                .frame(height: 200)
             }
-            .onAppear() {
-                apiCall().getRecipe(query: defaultText) { (foods) in
-                    self.foods = foods
-                }
+        }
+        .onAppear() {
+            apiCall().getRecipe(query: defaultText) { (foods) in
+                self.foods = foods
             }
-            .onChange(of: searchText) {
-                oldText, newText in
-                apiCall().getRecipe(query: newText) { (foods) in
-                    self.foods = foods
-                }
+        }
+        .onChange(of: searchText) {
+            oldText, newText in
+            apiCall().getRecipe(query: newText) { (foods) in
+                self.foods = foods
             }
         }
     }
+    
+    private func toggleLikedState(for index: Int) {
+        foods[index].isLiked.toggle() // Toggle liked state for the specific item
+    }
 }
+
 struct RecipeView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeView()
